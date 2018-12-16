1525eb1dc19b4df72c7ca54c39eb602ca7d95052
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-16 19:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code your solution in this file
 
 
-function findMatching(drivers, string){ return drivers.filter(item =>  item == string)}
+//function findMatching(drivers, string){ return drivers.filter(item =>  item == string)}
+
+function findMatching(drivers, string){ return drivers.filter(item => item.substring(1) == string.substring(1))}
 
 
 function fuzzyMatch(drivers, string){ return drivers.filter(function (x) {if (string === x.charAt(0)){return x}})}
