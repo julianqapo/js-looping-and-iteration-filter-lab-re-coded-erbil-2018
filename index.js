1120bccae81e1f7acecf2c00e417644a191aab77
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-16 20:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 function findMatching(drivers, string){ return drivers.filter(item => item.substring(1) == string.substring(1))}
 
 
-function fuzzyMatch(drivers, string){ return drivers.filter(function (x) {if (string === x.charAt(0) ){return x}})}
+//function fuzzyMatch(drivers, string){ return drivers.filter(function (x) {if (string === x.charAt(0) ){return x}})}
+function fuzzyMatch(drivers, string){ return drivers.filter(item => string == item.substring(0,2))}
 
  //function findMatching(drivers, string){ return drivers.filter(item => item.substring(0,2) == string.substring(0,2))}
  //function fuzzyMatch(drivers, string){ return drivers.filter(function (x) {if (string === x.charAt(0) || item.substring(0,2) == string.substring(0,2) ){return x}})}
