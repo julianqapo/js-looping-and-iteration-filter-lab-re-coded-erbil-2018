704ea4d28b87fc4be3f14434da74b88fe5e2cbe0
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-16 19:31:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+
+
+function findMatching(drivers, string){ return drivers.filter(item =>  item == string)}
+
+
+function fuzzyMatch(drivers, string){ return drivers.filter(function (x) {if (string === x.charAt(0)){return x}})}
+
+
+function matchName(drivers, string){return drivers.filter(function (x) {if (string === x.name.charAt(0)){return x}})}
